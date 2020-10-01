8e22067e6d85dcde3022ec4936dfa6878a67f20b
Riley Borgard <rileyborgard@gmail.com>
2020-10-01 16:08:10 -0400

    Submit button adds blog to local MongoDB database

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,9 @@ app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = "mongodb://localhost:27017/mydb";
+
 app.get('/writeblog/', (request, response) => {
     response.set('Cache-control', 'public, max-age=300, s-maxage=600');
     response.render('writeblog');
@@ -16,6 +19,21 @@ app.get('/writeblog/', (request, response) => {
 app.get('/', (request, response) => {
     response.set('Cache-control', 'public, max-age=300, s-maxage=600');
     response.render('index');
+});
+
+app.post('/postblog/', (request, response) => {
+    var blogObject = request.body;
+    console.log(blogObject);
+    MongoClient.connect(mongoURL, (err, db) => {
+        if(err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("blogs").insertOne(blogObject, (err, res) => {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+    response.redirect('/');
 });
 
 exports.app = functions.https.onRequest(app);
